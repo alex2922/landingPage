9f08b12ba23de0161322a21bb873f3927c948f1b
Harsh Singh <alexlin2922@gmail.com>
2024-12-27 18:10:20 +0530

    main

--- src/comps/hero/Hero.jsx
+++ src/comps/hero/Hero.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./Hero.scss";
+import { motion } from "framer-motion";
 
 function Hero() {
   const futureTexts = ["Futures", "Experiences", "Legacies"];
@@ -18,27 +19,115 @@ function Hero() {
     <div className="parent hero magicpattern">
       <div className="hero-container container">
         <div className="hero-content">
+          <motion.div className="icon bg-img-cover"  animate={{
+          y: [0, -10, 0, 10, 0], // Moves up and down
+          x: [0, 10, 0, -10, 0], // Moves side to side
+        }}
+        transition={{
+          duration: 6, // Duration of one complete cycle
+          repeat: Infinity, // Loops indefinitely
+          ease: "easeInOut",
+        }}>
+
+          </motion.div>
           <h1>
             <div className="line">
-              <span className="we">we</span>
-              <span className="build">BUILD</span>
+              <motion.span
+                className="we"
+                animate={{
+                  scale: [1, 1.05, 1],
+                  y: [0, -2, 0],
+                }}
+                transition={{
+                  duration: 2,
+                  repeat: Infinity,
+                  repeatDelay: 3,
+                  ease: "easeInOut",
+                }}
+              >
+                we
+              </motion.span>
+              <motion.span
+                className="build"
+                animate={{
+                  rotate: [0, 2, -2, 0],
+                }}
+                transition={{
+                  duration: 2.5,
+                  repeat: Infinity,
+                  repeatDelay: 2,
+                  ease: "easeInOut",
+                }}
+              >
+                BUILD
+              </motion.span>
             </div>
 
             <div className="line">
-              <span
+              <motion.span
                 className="future"
+                key={currentTextIndex} // Trigger re-render on text change
                 style={{
                   display: "inline-block",
+                  transformOrigin: "top center", // Set top border as the anchor
+                }}
+                initial={{ opacity: 0, rotateX: -90 }}
+                animate={{ opacity: 1, rotateX: 0 }}
+                exit={{ opacity: 0, rotateX: 90 }}
+                transition={{
+                  duration: 0.8,
+                  ease: "easeInOut",
                 }}
               >
                 {futureTexts[currentTextIndex]}
-              </span>
-              <span className="not">Not</span>
+              </motion.span>
+              <motion.span
+                className="not"
+                animate={{
+                  rotateX: [0, 10, 0],
+                }}
+                transition={{
+                  duration: 3,
+                  repeat: Infinity,
+                  repeatDelay: 3,
+                  ease: "easeInOut",
+                }}
+              >
+                Not
+              </motion.span>
             </div>
 
             <div className="line">
-              <span className="just">Just</span>
-              <span className="websites">Websites</span>
+              <motion.span
+                className="just"
+                animate={{
+                  scale: [1, 1.1, 1],
+                  x: [0, -2, 2, 0],
+                }}
+                transition={{
+                  duration: 3,
+                  repeat: Infinity,
+                  repeatDelay: 2.5,
+                  ease: "easeInOut",
+                }}
+              >
+                Just
+              </motion.span>
+              <motion.span
+                className="websites"
+                animate={{
+                  rotate: [0, 5, -5, 0],
+                  scale: [1, 1.05, 1],
+                }}
+                transition={{
+                  duration: 2.5,
+                  repeat: Infinity,
+                  repeatDelay: 2,
+                  ease: "easeInOut",
+                }}
+              >
+                Websites
+              </motion.span>
             </div>
           </h1>
         </div>
